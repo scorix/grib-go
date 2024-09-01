251e6dddb30bb5782b65ac74e9f195d403698298
scorix <scorix@gmail.com>
2024-09-02 00:10:07 +0800

    refactor: delete useless functions (#3)

--- pkg/grib2/scale/factor.go
+++ pkg/grib2/scale/factor.go
@@ -1,21 +1,6 @@
 package scale
 
-import (
-	"encoding/binary"
-	"io"
-)
-
 type Factor uint16
-
-func ReadScaleFactor(r io.Reader) (Factor, error) {
-	var f Factor
-
-	if err := binary.Read(r, binary.BigEndian, &f); err != nil {
-		return f, err
-	}
-
-	return f, nil
-}
 
 // https://codes.ecmwf.int/grib/format/grib2/regulations/
 // 92.1.5 If applicable, negative values shall be indicated by setting the most significant bit to “1”.
