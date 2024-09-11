4c7c36310cf5e18aa47792abbf17ead86d4cc301
scorix <scorix@gmail.com>
2024-09-11 12:30:38 +0800

    chore: make the error more clearly (#13)

--- pkg/grib2/drt/grid_point/simple_test.go
+++ pkg/grib2/drt/grid_point/simple_test.go
@@ -36,14 +36,14 @@ func TestSimpleScaleReader(t *testing.T) {
 	bs := []byte{0xff, 0xff, 0xff, 0b1010_0101, 0b1110_0000}
 	bf := bytes.NewReader(bs)
 
-	sp := gridpoint.NewSimplePacking(definition.SimplePacking{}, 1)
+	sp := gridpoint.NewSimplePacking(definition.SimplePacking{}, 3)
 	sp.R = 0.0194875
 	sp.E = -18
 	sp.D = -4
 	sp.Bits = 12
 	t.Logf("simple packing: %+v", sp)
 
-	r := gridpoint.NewSimplePackingReader(bf, 0, sp)
+	r := gridpoint.NewSimplePackingReader(bf, 0, 5, sp)
 	f, err := r.ReadGridAt(2)
 	require.NoError(t, err)
 	assert.InDelta(t, float32(2.9611706734e+02), f, 1e-5)
