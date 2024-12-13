4f26dab1eb1c916317c07643f11527650c7a6b0b
scorix <scorix@gmail.com>
2024-12-13 10:26:06 +0800

    fix: use a new store for different messages (#45)

--- pkg/grib2/message.go
+++ pkg/grib2/message.go
@@ -261,15 +261,15 @@ func NewSimplePackingMessageReaderFromMessage(r io.ReaderAt, m IndexedMessage, o
 
 type SimplePackingMessageReaderOptions func(r *simplePackingMessageReader)
 
-func WithBoundaryCache(minLat, maxLat, minLon, maxLon float32, store cache.Store) SimplePackingMessageReaderOptions {
+func WithBoundaryCache(minLat, maxLat, minLon, maxLon float32, newStore func() cache.Store) SimplePackingMessageReaderOptions {
 	return func(r *simplePackingMessageReader) {
-		r.cache = cache.NewBoundary(minLat, maxLat, minLon, maxLon, r.spr, store)
+		r.cache = cache.NewBoundary(minLat, maxLat, minLon, maxLon, r.spr, newStore())
 	}
 }
 
-func WithCustomCacheStrategy(inCache func(lat, lon float32) bool, store cache.Store) SimplePackingMessageReaderOptions {
+func WithCustomCacheStrategy(inCache func(lat, lon float32) bool, newStore func() cache.Store) SimplePackingMessageReaderOptions {
 	return func(r *simplePackingMessageReader) {
-		r.cache = cache.NewCustom(inCache, r.spr, store)
+		r.cache = cache.NewCustom(inCache, r.spr, newStore())
 	}
 }
 
